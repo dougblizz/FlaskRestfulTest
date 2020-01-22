52cfc516ab380257eda847f06cf2a11d0b6fd884
douglas.ocando <douglas.ocando@zentagroup.com>
2020-01-22 10:03:01 -0600

    update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, Items
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MyDB.db'
@@ -31,9 +30,11 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
 api.add_resource(StoreList, '/stores')
+
 api.add_resource(UserRegister, '/register')
+
 if __name__ == '__main__':
-    
+    from db import db
     db.init_app(app)
     app.run(port = 5000, debug = True)
 
