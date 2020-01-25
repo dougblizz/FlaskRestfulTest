3ee87b6dbcdbfa08913b4367ee3f86f4862333db
douglas.ocando <douglas.ocando@zentagroup.com>
2020-01-24 19:30:22 -0600

    fix

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ api.add_resource(Items, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
-api.add_resource(UserLogout, 'logout')
+api.add_resource(UserLogout, '/logout')
 api.add_resource(TokenRefresh, '/refresh')
 
 api.add_resource(UserRegister, '/register')
